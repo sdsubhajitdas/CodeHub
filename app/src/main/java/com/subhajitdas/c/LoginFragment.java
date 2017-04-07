cf6a84c35ecb5b6bfc52cd36f385fbbb48b0aa4b
Subhajit Das <jeetudas95@gmail.com>
2017-04-08 01:14:55 +0530

    Register UI changed and code adjusted

--- app/src/main/java/com/subhajitdas/c/LoginFragment.java
+++ app/src/main/java/com/subhajitdas/c/LoginFragment.java
@@ -10,7 +10,6 @@ import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
@@ -160,7 +159,7 @@ public class LoginFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login2, container, false);
+        return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
     @Override
--- app/src/main/java/com/subhajitdas/c/RegisterFragment.java
+++ app/src/main/java/com/subhajitdas/c/RegisterFragment.java
@@ -1,20 +1,20 @@
 package com.subhajitdas.c;
 
 
-import android.app.FragmentManager;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -29,19 +29,13 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterFragment extends Fragment {
 
-    EditText mEmail, mPassword, mUsername;
-    FloatingActionButton mGo;
-    ImageView mBack;
-    ProgressDialog mProgress;
+    private EditText mEmail, mPassword, mUsername;
+    private Button mRegister;
+    private ProgressDialog mProgress;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-
-    private String EMAIL = "email";
-    private String PASSWORD = "password";
-    private String USERNAME = "username";
     private String TAG = "Jeetu";
 
     public RegisterFragment() {
@@ -58,9 +52,9 @@ public class RegisterFragment extends Fragment {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
 
-        final DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("user");
+        final DatabaseReference userRef = FirebaseDatabase.getInstance().getReference(Constants.USER);
 
-        //----USER LOGIN LISTENER-----
+        // User login listener
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -70,9 +64,9 @@ public class RegisterFragment extends Fragment {
                     // User is signed in
                     //Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
                     mProgress.setMessage("Setting up your account ");
-                    userRef.child(user.getUid()).child("username").setValue(mUsername.getText().toString());
-                    userRef.child(user.getUid()).child("email").setValue(mEmail.getText().toString());
-                    userRef.child(user.getUid()).child("password").setValue(mPassword.getText().toString());
+                    userRef.child(user.getUid()).child(Constants.USERNAME).setValue(mUsername.getText().toString());
+                    userRef.child(user.getUid()).child(Constants.EMAIL).setValue(mEmail.getText().toString());
+                    userRef.child(user.getUid()).child(Constants.PASSWORD).setValue(mPassword.getText().toString());
 
                     UserProfileChangeRequest userProfileChangeRequest = new UserProfileChangeRequest.Builder()
                             .setDisplayName(mUsername.getText().toString())
@@ -82,9 +76,8 @@ public class RegisterFragment extends Fragment {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if (task.isSuccessful()) {
-
                                 mProgress.dismiss();
-
+                                // Changing activity.
                                 Intent intent = new Intent(getActivity(), MainActivity.class);
                                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -92,10 +85,7 @@ public class RegisterFragment extends Fragment {
                             }
                         }
                     });
-
-
                 }
-
             }
         };
 
@@ -103,18 +93,16 @@ public class RegisterFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_register2, container, false);
+        return inflater.inflate(R.layout.fragment_register, container, false);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
         mUsername = (EditText) getActivity().findViewById(R.id.register_username);
         mEmail = (EditText) getActivity().findViewById(R.id.register_email);
         mPassword = (EditText) getActivity().findViewById(R.id.register_password);
-        mGo = (FloatingActionButton) getActivity().findViewById(R.id.register_fab);
-        mBack = (ImageView) getActivity().findViewById(R.id.back_image);
+        mRegister = (Button) getActivity().findViewById(R.id.register_button);
         mProgress = new ProgressDialog(getActivity());
     }
 
@@ -127,11 +115,11 @@ public class RegisterFragment extends Fragment {
     @Override
     public void onViewStateRestored(Bundle savedInstanceState) {
         super.onViewStateRestored(savedInstanceState);
-
+        // Restored data is recovered.
         if (savedInstanceState != null) {
-            mUsername.setText(savedInstanceState.getString(USERNAME));
-            mEmail.setText(savedInstanceState.getString(EMAIL));
-            mPassword.setText(savedInstanceState.getString(PASSWORD));
+            mUsername.setText(savedInstanceState.getString(Constants.USERNAME));
+            mEmail.setText(savedInstanceState.getString(Constants.EMAIL));
+            mPassword.setText(savedInstanceState.getString(Constants.PASSWORD));
         }
     }
 
@@ -139,11 +127,10 @@ public class RegisterFragment extends Fragment {
     public void onResume() {
         super.onResume();
 
-        /*
-        mGo.setOnClickListener(new View.OnClickListener() {
+        mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // Data not empty
                 if (!(TextUtils.isEmpty(mEmail.getText().toString())) &&
                         !(TextUtils.isEmpty(mPassword.getText().toString())) &&
                         !(TextUtils.isEmpty(mUsername.getText().toString()))) {
@@ -158,26 +145,22 @@ public class RegisterFragment extends Fragment {
                                     //Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
 
                                     if (!task.isSuccessful()) {
-                                        Toast.makeText(getActivity(), "Cant create account.", Toast.LENGTH_SHORT).show();
+                                        if (Build.VERSION.SDK_INT >= 21)
+                                            Snackbar.make(getActivity().findViewById(android.R.id.content), "Cant create account", Snackbar.LENGTH_SHORT).show();
+                                        else
+                                            Toast.makeText(getActivity(), "Cant create account", Toast.LENGTH_SHORT).show();
                                         mProgress.dismiss();
                                     }
-
                                 }
                             });
-                }
-                else {
-                    Toast.makeText(getActivity(),"Please fill in details",Toast.LENGTH_SHORT).show();
+                } else {
+                    if (Build.VERSION.SDK_INT >= 21)
+                        Snackbar.make(getActivity().findViewById(android.R.id.content), "Please fill in details", Snackbar.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(getActivity(), "Please fill in details", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-        mBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager tempManager = getActivity().getFragmentManager();
-                tempManager.popBackStack();
-            }
-        });*/
     }
 
     @Override
@@ -195,10 +178,10 @@ public class RegisterFragment extends Fragment {
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
-        outState.putString(USERNAME, mUsername.getText().toString());
-        outState.putString(EMAIL, mEmail.getText().toString());
-        outState.putString(PASSWORD, mPassword.getText().toString());
-
+        // Storing the data before pausing.
+        outState.putString(Constants.USERNAME, mUsername.getText().toString());
+        outState.putString(Constants.EMAIL, mEmail.getText().toString());
+        outState.putString(Constants.PASSWORD, mPassword.getText().toString());
         super.onSaveInstanceState(outState);
     }
 }
